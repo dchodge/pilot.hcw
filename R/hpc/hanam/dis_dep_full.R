fa62b34ab3af9e819dbbfa035b6fbf07c59b3594
David  Hodgson <lshdh2@comp25.cluster>
2022-01-09 15:18:39 +0000

    HPC OUTPUT: 090122

--- R/hpc/hanam/dis_dep_full.R
+++ R/hpc/hanam/dis_dep_full.R
@@ -16,34 +16,13 @@ library(here)
 
 devtools::load_all() # hcwpre
 load(here::here("data", "hanam_data.RDS")) # hcwpre
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 load(file = here::here("data", paste0("modelinfo_cross_", hanam$study_name_short, ".RDS"))) # all_models_hcw_pre
 
 setup_run_serosolver(
         all_models_hanam_cross[[2]],
-=======
->>>>>>> 94e96f5e2a85087c694677fff34ac5732ae0bf9d
-load(file = here::here("data", paste0("modelinfo_cross_", hcwpre$study_name_short, ".RDS"))) # all_models_hcw_pre
-
-setup_run_serosolver(
-        all_models_hcw_pre_cross[[2]],
-<<<<<<< HEAD
-=======
->>>>>>> 418e98c3cfcb569d0d803ab7f898932712d6affa
->>>>>>> 94e96f5e2a85087c694677fff34ac5732ae0bf9d
         chains = 4,
         iterations = 1000000,
         pt = TRUE,
         filename = "dis_dep",
         cross_sectional = FALSE,
-<<<<<<< HEAD
         continue_run = FALSE)
-=======
-<<<<<<< HEAD
-        continue_run = FALSE)
-=======
-        continue_run = FALSE)
->>>>>>> 418e98c3cfcb569d0d803ab7f898932712d6affa
->>>>>>> 94e96f5e2a85087c694677fff34ac5732ae0bf9d
